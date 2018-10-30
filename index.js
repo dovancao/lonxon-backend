66b62775c1f6760f8a6989b75f1936d3ba237cb6
dovancao <sondp.solution@gmail.com>
2018-10-30 13:45:15 +0700

    fix transfer by web3 to  using ethereumjstx

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const WalletRouter = require('./modules/api/wallet/router');
 const BlockChainDataRouter = require('./modules/api/blockchain/router');
 const BigDataRouter = require('./modules/api/bigdata/router')
 
+//ethereum tx
+const EthereumTx = require('ethereumjs-tx');
+
 const app = express();
 
 app.use(cors());
@@ -32,10 +35,16 @@ app.listen(port, (err) => {
 const LoyaSolcJson = require('./contracts/build/contracts/LoyaToken.json');
 
 const LoyaSolc = new web3.eth.Contract(LoyaSolcJson.abi, LoyaSolcJson.networks[NETWORK].address);
+// determine the nonce
+// var count = new web3.eth.getBlockTransactionCount("0xd463664728702166C5256b30E91E5Da1c9bB43f8");
 let LoyaMethodAbi;
-
 Promise.resolve().then(() => {
   return LoyaSolc.methods.balanceOf('0xDbf16F1D0A2d75FD45A791e3E032938628a79664').call().then((balance) => {
+    var toWei = Math.pow(10, 18);
+    var balance = balance.toString(10)
+    console.log(balance/toWei);
+})}).then(() => {
+  return LoyaSolc.methods.balanceOf('0xd463664728702166C5256b30E91E5Da1c9bB43f8').call().then((balance) => {
     var toWei = Math.pow(10, 18);
     var balance = balance.toString(10)
     console.log(balance/toWei);
@@ -44,13 +53,31 @@ Promise.resolve().then(() => {
     console.log(_);
   })
 })
-.catch(console.log)
-
 .then(() => {
-  return LoyaSolc.methods.transfer('0xDbf16F1D0A2d75FD45A791e3E032938628a79664', 1000).call()
+
+  const rawTransaction = {
+    from: '0xd463664728702166C5256b30E91E5Da1c9bB43f8',
+    gasPrice: "0x09184e72a000",
+    gasLimit: "0x2710",
+    nonce: '0x00',
+    to: LoyaSolcJson.networks[NETWORK].address,
+    data: LoyaSolc.methods.transferFrom('0xd463664728702166C5256b30E91E5Da1c9bB43f8','0xDbf16F1D0A2d75FD45A791e3E032938628a79664', 1000).encodeABI(),
+    chainId: 3
+  }
+  var privKey = new Buffer.from('876E2D2983A0D162060272E2E31BAD9EF81EDA64680D4EFAC8031624B95C849A', 'hex');
+  const tx = new EthereumTx(rawTransaction);
+  tx.sign(privKey);
+  const seriallizedTx = tx.serialize();
+
+  // send transaction
+  web3.eth.sendSignedTransaction('0x' + seriallizedTx.toString('hex'), function(err, hash){
+    if (!err) console.log(hash)
+    console.log('error : ', err)
+  })
 }).then(() => {
   return LoyaSolc.methods.balanceOf('0xDbf16F1D0A2d75FD45A791e3E032938628a79664').call().then((balance) => {
     var toWei = Math.pow(10, 18);
     var balance = balance.toString(10)
     console.log(balance/toWei);
 })})
+.catch(console.log)
--- modules/api/wallet/router.js
+++ modules/api/wallet/router.js
@@ -1,34 +1,10 @@
 const express = require('express');
-const  Web3 = require('web3');
 const NETWORK =3;
 const defaultConfig = require('../../../config');
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 const keccak256 = require('js-sha3').keccak256;
-const { EthWallet,CoinType, InfinitoApi } = require('node-infinito-wallet');
-const web3 = new Web3(defaultConfig.url);
 const SmartContractAddress = '0x0e6bd10385c4b9c04687623e35569b7d5423b327';
-const opts = {
-     apiKey: '28922118-c15d-4694-8836-b9ab18741e37',
-     secret: 'dPdqS8KrjwAheOdh7FhnGyPTIxebDrLZ0rBbEypJuFiGroTvNAzFUwCTaqiRjSjb',
-     baseUrl: 'https://staging-api-testnet.infinitowallet.io',
-     logLevel: 'NONE',
- };
- const api = new InfinitoApi(opts);
- const coinAPI = api.ETH;
-
-//loyajson
-const LoyaSolcJson = require('../../../contracts/build/contracts/LoyaToken.json')
-const LoyaSolc = new web3.eth.Contract(LoyaSolcJson.abi, LoyaSolcJson.networks[NETWORK].address);
-// api config
-let apiConfig = {
-  apiKey: '28922118-c15d-4694-8836-b9ab18741e37',
-  secret: 'dPdqS8KrjwAheOdh7FhnGyPTIxebDrLZ0rBbEypJuFiGroTvNAzFUwCTaqiRjSjb',
-  baseUrl: 'https://staging-api-testnet.infinitowallet.io',
-  logLevel: 'NONE'
-}
-
-
 let Router = express.Router();
 
 Router.post('/new-wallet', function(req,res){
